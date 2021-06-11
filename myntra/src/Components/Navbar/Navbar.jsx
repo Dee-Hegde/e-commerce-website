6b32c7972b91de4c6547079c83367efe49a251bf
Shanoor24 <shanoorfb2403@gmail.com>
2021-06-11 11:49:33 +0530

    some changes made

--- myntra/src/Components/Navbar/Navbar.jsx
+++ myntra/src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,6 @@ function Navbar() {
             <NavLinks/>
             <NavSearch/>
             <NavRight/> 
-            
         </div>
     )
 }
--- myntra/src/Components/T-Shirt/Header.jsx
+++ myntra/src/Components/T-Shirt/Header.jsx
@@ -4,9 +4,14 @@ import styles from "./Header.module.css"
 function Header() {
     return (
         <div id={styles.main_div}>
-            
+            <div className={styles.title_div}>
+                <div>Home /</div>
+                <div>Clothing /</div>
+                <div>Men T-Shirts</div>
+            </div>
+            <div className={styles.items_count}>men T-shirts <span> - 54255 items</span></div>
         </div>
     )
 }
 
-export default Header;
+export default Header;   
