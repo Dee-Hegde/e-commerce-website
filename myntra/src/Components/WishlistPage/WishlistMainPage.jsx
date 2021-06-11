d0303512c939c1551da0bbc18ccec3adb54cfd72
unknown <sahua5979@gmail.com>
2021-06-11 18:52:07 +0530

    Updated Carousel in men's page

---
+++ myntra/src/Components/WishlistPage/WishlistMainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SubNavbar from './SubNavbar';
+
+const WishlistMainPage = () => {
+    return (
+        <div>
+            <SubNavbar/>
+        </div>
+    );
+};
+
+export default WishlistMainPage;
