0601e5589bc53b5bf62b505a3566389ed17f3417
Dee-Hegde <dee.sirsi@gmail.com>
2021-06-11 12:54:39 +0530

    Corrosals

--- myntra/src/index.js
+++ myntra/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Provider } from 'react-redux';
 import store from './Redux/Store';
 
