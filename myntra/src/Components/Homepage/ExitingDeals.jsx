16603ee9203e42ebc548268a6b90f2709fbd9cf8
Dee-Hegde <dee.sirsi@gmail.com>
2021-06-12 13:58:11 +0530

    HomePage added

--- myntra/src/Components/Homepage/ExitingDeals.jsx
+++ myntra/src/Components/Homepage/ExitingDeals.jsx
@@ -5,14 +5,20 @@ import Modarapido from "./Homepage Images/ModarapidoOffers.png"
 import Triumph from "./Homepage Images/TriomphOffers.png"
 import EleOffers from "./Homepage Images/EleOffers.png"
 import styles from "./Home.module.css"
+import Spotlight from "./Homepage Images/SpotLightImg.png"
+import TopOffers from "./Homepage Images/topOffers.png"
 
 function ExitingDeals() {
     let deals=[JJoffers, ArrowOffers,Modarapido ,Triumph,EleOffers]
     return (
-        <div className={styles.dealsdiv}>
-            {deals.map((item)=>(
-                <img src={item} alt="image" />
-            ))}
+        <div>
+            <div className={styles.dealsdiv}>
+                {deals.map((item)=>(
+                    <img src={item} alt="image" />
+                ))}
+            </div>
+            <img className={styles.spotimg} src={Spotlight} alt="image" /><br/>
+            <img className={styles.spotimg2} src={TopOffers} alt="image" />
         </div>
     )
 }
