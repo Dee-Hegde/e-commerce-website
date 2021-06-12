7bbab543ae3a015a0169e46a0d7a8ccd0f0bcbb8
Shanoor24 <shanoorfb2403@gmail.com>
2021-06-12 11:58:10 +0530

    added Productt listing component

--- myntra/src/Components/T-Shirt/ProductListComp.jsx
+++ myntra/src/Components/T-Shirt/ProductListComp.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getData } from "../../Redux/ProductListing/action";
 import styles from "./ProductListComp.module.css"
 
 
 function ProductListComp() {
     const dispatch = useDispatch();
+    const {data} = useSelector((state) => state.products)
 
     useEffect(() => {
         dispatch(getData())
@@ -13,9 +14,21 @@ function ProductListComp() {
 
     return (
         <div id={styles.main_wrapper}>
-            <div>
-
-            </div>
+            {
+                data.map((item) => <div className={styles.item_div}>
+                    <div><img src={item.images[0]}  alt=""  className={styles.item_image}/></div>
+                    <div>
+                        <div className={styles.item_title}>{item.title}</div>
+                        <div className={styles.item_sub_heading}>{item.sub_heading}</div>
+                        
+                        {item.discount === "0" ? <div className={styles.price}>Rs. {item.price}</div> : <div className={styles.item_price_div}>
+                            <div>Rs. {Math.floor(item.price * item.discount / 100)}</div>
+                            <div>Rs. {item.price}</div>
+                            <div>{`(${item.discount}% OFF)`}</div>
+                        </div>}
+                    </div>
+                    </div> )
+            }
         </div>
     )
 }
