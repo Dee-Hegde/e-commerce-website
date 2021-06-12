860ad7389f028af8f6a57339131b9f5a2e5675f0
Shanoor24 <shanoorfb2403@gmail.com>
2021-06-12 12:00:26 +0530

    some changes made

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import { TShirtMainPage } from './Components/T-Shirt/T_shirt_main_page';
-// import Routes from './Routing/Routes';
+import Routes from './Routing/Routes';
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes /> */}
-      <TShirtMainPage />
+      <Routes />
     </div>
   );
 }
