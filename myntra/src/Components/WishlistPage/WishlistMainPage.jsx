b4041e7251b3a3aac2d91bd40fc308bfa3cdf9eb
unknown <sahua5979@gmail.com>
2021-06-12 13:54:04 +0530

    adding wishlist page

---
+++ myntra/src/Components/WishlistPage/WishlistMainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyWishlist from './MyWishlist';
+import SubNavbar from './SubNavbar';
+
+const WishlistMainPage = () => {
+    return (
+        <div>
+            <SubNavbar/>
+            <MyWishlist/>
+        </div>
+    );
+};
+
+export default WishlistMainPage;
