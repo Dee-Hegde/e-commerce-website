d8f463b3375a877fbb119157981bd1969e9ff882
Dee-Hegde <dee.sirsi@gmail.com>
2021-06-13 17:34:10 +0530

    Homepage Complete

--- myntra/src/Components/Homepage/ExitingDeals.jsx
+++ myntra/src/Components/Homepage/ExitingDeals.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import JJoffers from "./Homepage Images/JJOffers.png"
 import ArrowOffers from "./Homepage Images/ArrowOffers.png"
 import Modarapido from "./Homepage Images/ModarapidoOffers.png"
-import Triumph from "./Homepage Images/TriomphOffers.png"
+import Triumph from "./Homepage Images/ForHim/Beardo.png"
 import EleOffers from "./Homepage Images/EleOffers.png"
 import styles from "./Home.module.css"
 import Spotlight from "./Homepage Images/SpotLightImg.png"
