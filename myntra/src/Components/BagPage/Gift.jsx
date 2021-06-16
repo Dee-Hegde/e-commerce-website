2aa7a2d5b3e1126ebb578d05feb4d199c19cd4fb
unknown <sahua5979@gmail.com>
2021-06-16 10:45:51 +0530

    Updated bag page

---
+++ myntra/src/Components/BagPage/Gift.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import giftImage from "./Images/giftImage.png";
+import styles from "./bag.module.css";
+const Gift = () => {
+    return (
+        <div className={`${styles.marginTop} ${styles.borderBottom}`}>
+            <div className={`${styles.fontBold} ${styles.font14} ${styles.marginTop}`}>GIFTING AND PERSONILISATION</div>
+            <div className={` ${styles.marginTop}`}>
+                <img src={giftImage} alt="" width="100%" />
+            </div>
+        </div>
+    );
+};
+
+export default Gift;
