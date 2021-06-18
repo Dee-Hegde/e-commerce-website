13f40bf4aaf95cfb011efcd31c574a354d6c3af5
unknown <sahua5979@gmail.com>
2021-06-18 21:28:23 +0530

    Updated size select model in cart page

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Routes from './Routing/Routes';
-import ProductDetails from "./Components/ProductDetails/ProductDetails"
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes /> */}
-      <ProductDetails/>
+      <Routes />
 
     </div>
   );
