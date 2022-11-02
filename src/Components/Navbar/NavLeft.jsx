f1cd1f135eabea425dfa70a33481c2bab34bc71f
Dee-Hegde <deesirsi@gmail.com>
2022-11-02 22:27:29 +0530

    Logo changed

--- src/Components/Navbar/NavLeft.jsx
+++ src/Components/Navbar/NavLeft.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from "./navimages/Myntra-logo-png-ico.png"
+import Logo from "./navimages/logo.png"
 import styles from "./Navbar.module.css"
 import { useHistory } from "react-router-dom";
 
